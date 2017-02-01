e237c026cdb872448cefb1aef5fa55ad010eb641
Christoffer Hauthorn <christoffer@emplate.it>
2017-02-01 15:14:47 +0100

    Plotting with gnu

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <ctime>
 #include <fstream>
+#include <cstdlib>
 #include "Pred.cpp"
 
 using namespace std;
 
-const int N=10000;
-const int MAX=10000;
+const int N=10000000;
+const int MAX=10100000;
 
 int main() {
     std::ofstream outfile;
 
-    outfile.open("test.txt", std::ios_base::app);
 
-    for (int j = N; j <= MAX; j = j + 1000) {
+
+    for (int j = N; j <= MAX; j = j + 10000) {
         // Build an array of integers of size X
         int tmp = 0;
         vector<int> X(j);
@@ -28,18 +29,21 @@ int main() {
 
         // Run algorithm
         int pred = Pred::predWithScan(X, 89);
-        
-        cout << "Pred: " << pred << endl;
 
         // End timer
         clock_t end = clock();
         double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
 
+        cout << "Pred: " << pred << endl;
+
         // Output to tsv file
+        outfile.open("test.txt", std::ios_base::app);
         outfile << j << "\t" << elapsed_secs << endl;
+        outfile.close();
     }
 
-    // Cleanup
-    outfile.close();
+    // Print the plot
+    system("gnuplot -e \"set term png;set output 'test.png'; plot 'test.txt' with lines\"");
+
     return 0;
 }
