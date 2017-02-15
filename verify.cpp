a48cdafb08fa7102f014ab57c6757ca06c50b3d8
Christoffer Hauthorn <christoffer@hauthorn.me>
2017-02-15 13:50:40 +0100

    Beginning to implement the verify target

---
+++ verify.cpp
@@ -0,0 +1,13 @@
+//
+// Created by christoffer on 2/15/17.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "Verifying algorithms" << endl;
+
+    return 0;
+}
