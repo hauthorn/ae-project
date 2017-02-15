e0c653b9e39e7942a9892656cb687bd435c0438c
Christoffer Hauthorn <christoffer@hauthorn.me>
2017-02-15 15:11:29 +0100

    run_it_1.sh added

---
+++ run_it_1.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+mkdir -p cmake-build-debug;
+cd cmake-build-debug;
+cmake --build . --target ae_project -- -j 2;
+./ae_project -a linear -n 1000 -m br_msp -f it1;
+./ae_project -a linear -n 1000 -m l2_dcm -f it1;
+
+./ae_project -a binaryRecursive -n 1000 -m br_msp -f it1;
+./ae_project -a binaryRecursive -n 1000 -m l2_dcm -f it1;
+
+./ae_project -a binaryIterative -n 1000 -m br_msp -f it1;
+./ae_project -a binaryIterative -n 1000 -m l2_dcm -f it1;
