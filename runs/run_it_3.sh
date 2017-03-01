638fd7b29965bb55f2e283b5ef54d97f3cbe0607
anders <andersgm@outlook.dk>
2017-03-01 14:25:44 +0100

    added c..

--- runs/run_it_3.sh
+++ runs/run_it_3.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #mkdir -p cmake-build-debug;
 #cd cmake-build-debug;
+cd ..
 cmake --build . --target ae_project -- -j 2;
 
 # This fits in main memory of Anders' server
@@ -17,10 +18,3 @@ cmake --build . --target ae_project -- -j 2;
 ./ae_project -a binaryIterative -n 1000 -max 250000000 -m l2_dcm -f it3;
 #./ae_project -a binaryIterative -n 1000 -max 250000000 -m l3_dcm -f it3;
 #./ae_project -a binaryIterative -n 1000 -max 250000000 -m tlb_tl -f it3;
-
-./ae_project -a linear -n 1000 -max 250000000 -m br_msp -f it3;
-./ae_project -a linear -n 1000 -max 250000000 -m br_cn -f it3;
-./ae_project -a linear -n 1000 -max 250000000 -m l1_dcm -f it3;
-./ae_project -a linear -n 1000 -max 250000000 -m l2_dcm -f it3;
-#./ae_project -a linear -n 1000 -max 250000000 -m l3_dcm -f it3;
-#./ae_project -a linear -n 1000 -max 250000000 -m tlb_tl -f it3;
