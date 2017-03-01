993382e2f862ae08ae59d7e05859c55770fe00f6
anders <andersgm@outlook.dk>
2017-03-01 14:24:34 +0100

    added run it 3

---
+++ runs/run_it_3.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#mkdir -p cmake-build-debug;
+#cd cmake-build-debug;
+cmake --build . --target ae_project -- -j 2;
+
+# This fits in main memory of Anders' server
+./ae_project -a binaryRecursive -n 1000 -max 250000000 -m br_msp -f it3;
+./ae_project -a binaryRecursive -n 1000 -max 250000000 -m br_cn -f it3;
+./ae_project -a binaryRecursive -n 1000 -max 250000000 -m l1_dcm -f it3;
+./ae_project -a binaryRecursive -n 1000 -max 250000000 -m l2_dcm -f it3;
+#./ae_project -a binaryRecursive -n 1000 -max 250000000 -m l3_dcm -f it3;
+#./ae_project -a binaryRecursive -n 1000 -max 250000000 -m tlb_tl -f it3;
+
+./ae_project -a binaryIterative -n 1000 -max 250000000 -m br_msp -f it3;
+./ae_project -a binaryIterative -n 1000 -max 250000000 -m br_cn -f it3;
+./ae_project -a binaryIterative -n 1000 -max 250000000 -m l1_dcm -f it3;
+./ae_project -a binaryIterative -n 1000 -max 250000000 -m l2_dcm -f it3;
+#./ae_project -a binaryIterative -n 1000 -max 250000000 -m l3_dcm -f it3;
+#./ae_project -a binaryIterative -n 1000 -max 250000000 -m tlb_tl -f it3;
+
+./ae_project -a linear -n 1000 -max 250000000 -m br_msp -f it3;
+./ae_project -a linear -n 1000 -max 250000000 -m br_cn -f it3;
+./ae_project -a linear -n 1000 -max 250000000 -m l1_dcm -f it3;
+./ae_project -a linear -n 1000 -max 250000000 -m l2_dcm -f it3;
+#./ae_project -a linear -n 1000 -max 250000000 -m l3_dcm -f it3;
+#./ae_project -a linear -n 1000 -max 250000000 -m tlb_tl -f it3;
