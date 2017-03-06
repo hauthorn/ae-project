af98d42fad514d8a5ccb308897142e38224545c2
Christoffer Hauthorn <christoffer@emplate.it>
2017-03-06 12:17:44 +0100

    run 12 succesful. Used for report

--- compare-graphs.sh
+++ compare-graphs.sh
@@ -1,63 +1,28 @@
 #!/usr/bin/env bash
 
+prefix="set logscale x; set term png size 1200,800;set output 'runs/$1/$1_comparison"
+
 # Comparison of runtime
-gnuplot -e "plot 'runs/$1/$1_binite.txt' using 1:2 smooth frequency with points, \
-                'runs/$1/$1_bintree.txt' using 1:2 smooth frequency with points, \
-				'runs/$1/$1_binrec.txt' using 1:2 smooth frequency with points;
-			MAX=GPVAL_Y_MAX;
-    		MIN=GPVAL_Y_MIN;
-			set yrange [MIN-(MAX-MIN)*0.01:MAX+(MAX-MIN)*0.20];
-			set term png size 1200,800;set output 'runs/$1/$1_comparison_runtime.png';
-			set logscale x; set logscale y;
+gnuplot -e  "${prefix}_runtime.png'; set logscale y;
 			plot 'runs/$1/$1_binite.txt' using 1:2 smooth frequency with points, \
-			    'runs/$1/$1_bintree.txt' using 1:2 smooth frequency with points, \
 				'runs/$1/$1_binrec.txt' using 1:2 smooth frequency with points;"
 
 # Comparison of br cn
-gnuplot -e "plot 'runs/$1/$1_binite_br_cn.txt' using 1:2 smooth frequency with points, \
-                'runs/$1/$1_bintree_br_cn.txt' using 1:2 smooth frequency with points, \
-				'runs/$1/$1_binrec_br_cn.txt' using 1:2 smooth frequency with points;
-			MAX=GPVAL_Y_MAX;
-    		MIN=GPVAL_Y_MIN;
-			set yrange [MIN-(MAX-MIN)*0.01:MAX+(MAX-MIN)*0.20];
-			set term png size 1200,800;set output 'runs/$1/$1_comparison_br_cn.png';
-			set logscale x; set logscale y;
+gnuplot -e "${prefix}_br_cn.png';
 			plot 'runs/$1/$1_binite_br_cn.txt' using 1:2 smooth frequency with points, \
-			    'runs/$1/$1_bintree_br_cn.txt' using 1:2 smooth frequency with points, \
 				'runs/$1/$1_binrec_br_cn.txt' using 1:2 smooth frequency with points;"
 
-
 # Comparison of br msp
-gnuplot -e "plot 'runs/$1/$1_binite_br_msp.txt' using 1:2 smooth frequency with points, \
-                    'runs/$1/$1_binite_br_msp.txt' using 1:2 smooth frequency with points, \
-				'runs/$1/$1_binrec_br_msp.txt' using 1:2 smooth frequency with points;
-			MAX=GPVAL_Y_MAX;
-    		MIN=GPVAL_Y_MIN;
-			set yrange [MIN-(MAX-MIN)*0.01:MAX+(MAX-MIN)*0.20];
-			set term png size 1200,800;set output 'runs/$1/$1_comparison_br_msp.png';
-			set logscale x; set logscale y;
+gnuplot -e "${prefix}_br_msp.png';
 			plot 'runs/$1/$1_binite_br_msp.txt' using 1:2 smooth frequency with points, \
 				'runs/$1/$1_binrec_br_msp.txt' using 1:2 smooth frequency with points;"
 
-
 # Comparison of dcm
-gnuplot -e "plot 'runs/$1/$1_binite_l1_dcm.txt' using 1:2 smooth frequency with points, \
-				'runs/$1/$1_binrec_l1_dcm.txt' using 1:2 smooth frequency with points;
-			MAX=GPVAL_Y_MAX;
-    		MIN=GPVAL_Y_MIN;
-			set yrange [MIN-(MAX-MIN)*0.01:MAX+(MAX-MIN)*0.20];
-			set term png size 1200,800;set output 'runs/$1/$1_comparison_l1_dcm.png';
-			set logscale x; set logscale y;
+gnuplot -e "${prefix}_l1_dcm.png';
 			plot 'runs/$1/$1_binite_l1_dcm.txt' using 1:2 smooth frequency with points, \
 				'runs/$1/$1_binrec_l1_dcm.txt' using 1:2 smooth frequency with points;"
 
 # Comparison of l2 dcm
-gnuplot -e "plot 'runs/$1/$1_binite_l2_dcm.txt' using 1:2 smooth frequency with points, \
-				'runs/$1/$1_binrec_l2_dcm.txt' using 1:2 smooth frequency with points;
-			MAX=GPVAL_Y_MAX;
-    		MIN=GPVAL_Y_MIN;
-			set yrange [MIN-(MAX-MIN)*0.01:MAX+(MAX-MIN)*0.20];
-			set term png size 1200,800;set output 'runs/$1/$1_comparison_l2_dcm.png';
-			set logscale x; set logscale y;
+gnuplot -e "${prefix}_l2_dcm.png';
 			plot 'runs/$1/$1_binite_l2_dcm.txt' using 1:2 smooth frequency with points, \
 				'runs/$1/$1_binrec_l2_dcm.txt' using 1:2 smooth frequency with points;"
