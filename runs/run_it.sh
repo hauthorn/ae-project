e97993043c77c5fae6104df1acb46c1219ce529e
Christoffer Hauthorn <christoffer@emplate.it>
2017-03-07 12:29:18 +0100

    IT21 with BFS

--- runs/run_it.sh
+++ runs/run_it.sh
@@ -3,17 +3,17 @@ cd ..;
 cmake --build . --target ae_project -- -j 2;
 
 # This fits in main memory of Anders' server
-./ae_project -a binaryRecursive -n 10000 -max 10000000 -m br_msp -f $1;
-./ae_project -a binaryRecursive -n 10000 -max 10000000 -m br_cn -f $1;
-./ae_project -a binaryRecursive -n 10000 -max 10000000 -m l1_dcm -f $1;
-./ae_project -a binaryRecursive -n 10000 -max 10000000 -m l2_dcm -f $1;
+./ae_project -a binaryRecursive -n 10000 -max 250000000 -m br_msp -f $1;
+./ae_project -a binaryRecursive -n 10000 -max 250000000 -m br_cn -f $1;
+./ae_project -a binaryRecursive -n 10000 -max 250000000 -m l1_dcm -f $1;
+./ae_project -a binaryRecursive -n 10000 -max 250000000 -m l2_dcm -f $1;
 
-./ae_project -a binaryIterative -n 10000 -max 10000000 -m br_msp -f $1;
-./ae_project -a binaryIterative -n 10000 -max 10000000 -m br_cn -f $1;
-./ae_project -a binaryIterative -n 10000 -max 10000000 -m l1_dcm -f $1;
-./ae_project -a binaryIterative -n 10000 -max 10000000 -m l2_dcm -f $1;
+./ae_project -a binaryIterative -n 10000 -max 250000000 -m br_msp -f $1;
+./ae_project -a binaryIterative -n 10000 -max 250000000 -m br_cn -f $1;
+./ae_project -a binaryIterative -n 10000 -max 250000000 -m l1_dcm -f $1;
+./ae_project -a binaryIterative -n 10000 -max 250000000 -m l2_dcm -f $1;
 
-./ae_project -a binaryTree -n 10000 -max 10000000 -m br_msp -f $1;
-./ae_project -a binaryTree -n 10000 -max 10000000 -m br_cn -f $1;
-./ae_project -a binaryTree -n 10000 -max 10000000 -m l1_dcm -f $1;
-./ae_project -a binaryTree -n 10000 -max 10000000 -m l2_dcm -f $1;
+./ae_project -a binaryBFS -n 10000 -max 250000000 -m br_msp -f $1;
+./ae_project -a binaryBFS -n 10000 -max 250000000 -m br_cn -f $1;
+./ae_project -a binaryBFS -n 10000 -max 250000000 -m l1_dcm -f $1;
+./ae_project -a binaryBFS -n 10000 -max 250000000 -m l2_dcm -f $1;
