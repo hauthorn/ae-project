58562fa68a2506df40d1538013ca2a3cffb9a945
anders <andersgm@outlook.dk>
2017-03-10 14:28:51 +0100

    added one dimensional array mutliplication

--- matrix/matrix.cpp
+++ matrix/matrix.cpp
@@ -19,6 +19,7 @@ int **matrixMultiplySimple(int **a, int **b, int n) {
 
     for(int i=0; i < n; i++) {
         c[i] = new int[n];
+
         for (int j = 0; j < n; j++) {
             int temp = 0;
 
@@ -33,7 +34,12 @@ int **matrixMultiplySimple(int **a, int **b, int n) {
 }
 
 
-
+/**
+ * transpose an n*n matrix
+ * @param src source matrix. Src is not modified
+ * @param dst pointer to destination matrix
+ * @param N
+ */
 void transpose(int **src, int **dst, const int N) {
     for(int i = 0; i<N; i++) {
         for(int j = 0; j < N; j++) {
@@ -53,9 +59,7 @@ int **matrixMultiplySimpleTranspose(int **a, int **b, const int n) {
     int** c = 0;
     c = new int*[n];
 
-
     int **t = new int*[n];
-
 
     for(int i = 0; i<n;i++) {
         t[i] = new int[n];
@@ -69,11 +73,41 @@ int **matrixMultiplySimpleTranspose(int **a, int **b, const int n) {
             int temp = 0;
 
             for (int k = 0; k < n; k++)
-                temp += a[i][k] * t[j][k];
+                temp += a[i][k]*t[j][k];
 
             c[i][j] = temp;
         }
     }
 
     return c;
+}
+
+/**
+ * multiply vectors represented in 1 dimension
+ * @param a
+ * @param b
+ * @param n
+ * @return
+ */
+int *matrixOneDimension(int *a, int *b, const int n) {
+    int *c = new int[n];
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            int temp = 0;
+            for(int k = 0; k < n; k++) {
+                int aa = a[n*i+k];
+                int bb = b[k*n+j];
+                temp += aa*bb;
+            }
+
+            c[(n*i)+j] = temp;
+        }
+    }
+
+    return c;
+}
+
+int *matrixOneDimensionTranspose(int *a, int *b, const int n) {
+    return 0;
 }
--- matrix/verify.cpp
+++ matrix/verify.cpp
@@ -9,31 +9,116 @@
 using namespace std;
 
 int main() {
-    int **a, **b;
-    int n = 10;
+    bool printM = true;
+
+    int **a, **b, *a1, *b1;
+    int n = 3;
 
     a = new int *[n];
     b = new int *[n];
+    a1 = new int[n*n];
+    b1 = new int[n*n];
+
 
     for(int i = 0; i < n; i++) {
+        // init arrays
         a[i] = new int[n];
         b[i] = new int[n];
+
         for(int j = 0; j < n; j++) {
-            a[i][j] = rand()%n;
-            b[i][j] = rand()%n;
+            int i1 = rand() % n;
+            a[i][j] = i1;
+            a1[n*i+j] = i1;
+
+            int i2 = rand() % n;
+            b[i][j] = i2;
+            b1[(n*i)+j] = i2;
         }
+
     }
+
+    if(printM) {
+        // i is column, j is row
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                cout << a[i][j] << " ";
+            }
+
+            cout << endl;
+        }
+
+
+        cout << endl;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                cout << b[i][j] << " ";
+            }
+
+            cout << endl;
+        }
+
+        cout << " a1 " << endl;
+
+        for(int i = 0; i < n; i++) {
+            for(int j=0; j < n; j++) {
+                cout << a1[i*n+j] << " ";
+            }
+            cout << endl;
+        }
+
+        cout << endl;
+
+
+        cout << " b1 " << endl;
+
+        for(int i = 0; i < n; i++) {
+            for(int j=0; j < n; j++) {
+                cout << b1[i*n+j] << " ";
+            }
+            cout << endl;
+        }
+
+        cout << endl;
+
+
+        cout << endl;
+    }
+
 
     int **r1 = matrixMultiplySimple(a,b,n);
     int **r2 = matrixMultiplySimpleTranspose(a,b,n);
+    int *r3 = matrixOneDimension(a1,b1,n);
+    
+
 
 
     for(int i = 0; i<n; i++) {
         for(int j = 0; j < n; j++) {
             if(r1[i][j] != r2[i][j]) {
                 cout << "algorithms varies " << r1[i][j] << " " << r2[i][j] << endl;
-            } else {
+            }
+
+            if(r2[i][j] != r3[(i*n)+j]) {
+                cout << "algorithm 2 and 3 varies" << endl;
+            }
+        }
+    }
+
+    if(printM) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
                 cout << r1[i][j] << " ";
+            }
+
+            cout << endl;
+        }
+
+        cout << endl;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                cout << r3[n*i+j] << " ";
             }
 
             cout << endl;
