1d183b3a8659e2e50ca4bcab5296c78fd6f90467
anders <andersgm@outlook.dk>
2017-03-11 13:57:46 +0100

    added simpleTransposed as run

--- runs/run_it_matrix.sh
+++ runs/run_it_matrix.sh
@@ -10,13 +10,13 @@ cmake --build . --target matrixMultiply -- -j 2;
 
 #./matrixMultiply -a simpleTranspose -n 20 -max 1024 -m br_msp -f $1
 #./matrixMultiply -a simpleTranspose -n 20 -max 1024 -m br_cn -f $1
-./matrixMultiply -a simple -n 20 -max 1800 -m l1_dcm -f $1
-./matrixMultiply -a simple -n 20 -max 1800 -m l2_dcm -f $1
+./matrixMultiply -a simpleTranspose -n 20 -max 1800 -m l1_dcm -f $1
+./matrixMultiply -a simpleTranspose -n 20 -max 1800 -m l2_dcm -f $1
 
 #./matrixMultiply -a simpleWithTransposed -n 20 -max 1800 -m br_msp -f $1
 #./matrixMultiply -a simpleWithTransposed -n 20 -max 1800 -m br_cn -f $1
-./matrixMultiply -a simpleWithTransposed -n 20 -max 1800 -m l1_dcm -f $1
-./matrixMultiply -a simpleWithTransposed -n 20 -max 1800 -m l2_dcm -f $1
+#./matrixMultiply -a simpleWithTransposed -n 20 -max 1800 -m l1_dcm -f $1
+#./matrixMultiply -a simpleWithTransposed -n 20 -max 1800 -m l2_dcm -f $1
 #
 #
 #./matrixMultiply -a 1d -n 20 -max 1024 -m br_msp -f $1
