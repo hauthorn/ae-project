4cb09d38212d36e161618a7499ee3600c32f1665
anders <andersgm@outlook.dk>
2017-03-11 14:12:01 +0100

    updated run script simpleTranpose, 1d, 1dTranspose

--- runs/run_it_matrix.sh
+++ runs/run_it_matrix.sh
@@ -19,13 +19,13 @@ cmake --build . --target matrixMultiply -- -j 2;
 #./matrixMultiply -a simpleWithTransposed -n 20 -max 1800 -m l2_dcm -f $1
 #
 #
-#./matrixMultiply -a 1d -n 20 -max 1024 -m br_msp -f $1
-#./matrixMultiply -a 1d -n 20 -max 1024 -m br_cn -f $1
-#./matrixMultiply -a 1d -n 20 -max 1024 -m l1_dcm -f $1
-#./matrixMultiply -a 1d -n 20 -max 1024 -m l2_dcm -f $1
-#
-#
-#./matrixMultiply -a 1dTranspose -n 20 -max 1024 -m br_msp -f $1
-#./matrixMultiply -a 1dTranspose -n 20 -max 1024 -m br_cn -f $1
-#./matrixMultiply -a 1dTranspose -n 20 -max 1024 -m l1_dcm -f $1
-#./matrixMultiply -a 1dTranspose -n 20 -max 1024 -m l2_dcm -f $1
+#./matrixMultiply -a 1d -n 20 -max 1800 -m br_msp -f $1
+#./matrixMultiply -a 1d -n 20 -max 1800 -m br_cn -f $1
+./matrixMultiply -a 1d -n 20 -max 1800 -m l1_dcm -f $1
+./matrixMultiply -a 1d -n 20 -max 1800 -m l2_dcm -f $1
+
+
+#./matrixMultiply -a 1dTranspose -n 20 -max 1800 -m br_msp -f $1
+#./matrixMultiply -a 1dTranspose -n 20 -max 1800 -m br_cn -f $1
+./matrixMultiply -a 1dTranspose -n 20 -max 1800 -m l1_dcm -f $1
+./matrixMultiply -a 1dTranspose -n 20 -max 1800 -m l2_dcm -f $1
