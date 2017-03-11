a881f3b84c963b303bf0c39492fc2bbf2dd267b6
Christoffer Hauthorn <christoffer@emplate.it>
2017-03-11 10:26:22 +0100

    Adding kary heap to test again

--- heap/verify.cpp
+++ heap/verify.cpp
@@ -8,18 +8,19 @@
 #include <queue>
 #include "heap.cpp"
 #include "QueueHeap.cpp"
+#include "KaryHeap.cpp"
 
 
 using namespace std;
 
-const unsigned int size = 10;
+const unsigned int size = 1000;
 unsigned int* v = new unsigned int[size];
 
 int main() {
-
     vector<BaseHeap*> algorithms(0);
     algorithms.push_back(new Heap);
     algorithms.push_back(new QueueHeap);
+    algorithms.push_back(new KaryHeap());
 
     unsigned seed = (unsigned int) std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
@@ -36,7 +37,7 @@ int main() {
     }
 
 
-    // Do size / 2 extracts
+    // Do size extracts
     for (int j = 0; j < size; ++j) {
         unsigned int preResult = 0;
 
@@ -54,54 +55,7 @@ int main() {
         }
     }
 
-    for (BaseHeap *h : algorithms) {
-        h->insert(4);
-    }
-
-    for (BaseHeap *h : algorithms) {
-        h->insert(8);
-    }
-
-    for (BaseHeap *h : algorithms) {
-        h->insert(3);
-    }
-
-    for (BaseHeap *h : algorithms) {
-        h->insert(6);
-    }
-
-    for (int j = 0; j < algorithms.size(); j++) {
-        int i = 3;
-        if (algorithms.at(j)->heapExtractMin() != i) {
-            cout << "Not 3\n" << endl;
-        }
-    }
-
-    for (int j = 0; j < algorithms.size(); j++) {
-        int i = 4;
-        if (algorithms.at(j)->heapExtractMin() != i) {
-            cout << "Not 4\n" << endl;
-        }
-    }
-
-    for (int j = 0; j < algorithms.size(); j++) {
-        int i = 6;
-        if (algorithms.at(j)->heapExtractMin() != i) {
-            cout << "Not 6\n" << endl;
-        }
-    }
-
-    for (int j = 0; j < algorithms.size(); j++) {
-        int i = 8;
-        if (algorithms.at(j)->heapExtractMin() != i) {
-            cout << "Not 8\n" << endl;
-        }
-    }
-    
-    
-    return 0;
-
-    // Do size / 2 inserts
+    // Do size inserts
     for (int k = 0; k < size; ++k) {
         unsigned int value = (unsigned int) distribution(generator);
 
@@ -110,7 +64,7 @@ int main() {
         }
     }
 
-    // Do size / extracts
+    // Do size extracts
     for (int j = 0; j < size; ++j) {
         unsigned int preResult = 0;
 
