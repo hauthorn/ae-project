3efafcbcb450850707140b0aa1e2812e92e191dd
andersgm <andersgm@outlook.dk>
2017-03-12 23:57:19 +0100

    added recursive matrix multiplication + run

--- runs/run_it_matrixRecursive.sh
+++ runs/run_it_matrixRecursive.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd ..;
-cmake --build . --target matrixMultiply -- -j 2;
+cmake --build . --target matrixRecursive -- -j 2;
 
 # This fits in main memory of Anders' server
 #./matrixRecursive -n 20 -max 2048 -m l1_dcm -f $1;
