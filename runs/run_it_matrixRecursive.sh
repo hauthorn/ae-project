5e669a7ee056dfa05858586c7084b59a9b7b3eca
andersgm <andersgm@outlook.dk>
2017-03-15 17:47:04 +0100

    added run to measure insructions

--- runs/run_it_matrixRecursive.sh
+++ runs/run_it_matrixRecursive.sh
@@ -5,5 +5,5 @@ cmake --build . --target matrixRecursive -- -j 2;
 ./matrixRecursive -n 20 -max 2048 -m tot_iis -f $1;
 ./matrixRecursive -n 20 -max 2048 -m tlb_tl -f $1;
 
-./matrixMultiply -a simpleTranspose -n 20 -max 1800 -m tot_iis -f $1
-./matrixMultiply -a simpleTranspose -n 20 -max 1800 -m tlb_tl -f $1
+./matrixMultiply -a simpleTranspose -n 20 -max 2048 -m tot_iis -f $1
+./matrixMultiply -a simpleTranspose -n 20 -max 2048 -m tlb_tl -f $1
