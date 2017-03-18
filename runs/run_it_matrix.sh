85f29b61af154e591c3943db4e71eabc5946f661
andersgm <andersgm@outlook.dk>
2017-03-18 10:11:25 +0100

    opdated matrix tile

--- runs/run_it_matrix.sh
+++ runs/run_it_matrix.sh
@@ -3,18 +3,23 @@ cd ..;
 cmake --build . --target matrixMultiply -- -j 2;
 
 # This fits in main memory of Anders' server
+./matrixRecursive -n 5 -max 2048 -m l1_dcm -f $1;
+./matrixRecursive -n 5 -max 2048 -m l2_dcm -f $1;
+./matrixRecursive -n 5 -max 2048 -m tot_iis -f $1;
 
-./matrixMultiply -a simple -n 5 -max 2000 -m l1_dcm -f $1;
-./matrixMultiply -a simple -n 5 -max 2000 -m l2_dcm -f $1;
+./matrixMultiply -a simple -n 5 -max 2200 -m l1_dcm -f $1;
+./matrixMultiply -a simple -n 5 -max 2200 -m l2_dcm -f $1;
 
-./matrixMultiply -a simpleTranspose -n 5 -max 2000 -m l1_dcm -f $1
-./matrixMultiply -a simpleTranspose -n 5 -max 2000 -m l2_dcm -f $1
+./matrixMultiply -a simpleTranspose -n 5 -max 2200 -m l1_dcm -f $1
+./matrixMultiply -a simpleTranspose -n 5 -max 2200 -m l2_dcm -f $1
 
-./matrixMultiply -a 1dTranspose -n 5 -max 2000 -m l1_dcm -f $1
-./matrixMultiply -a 1dTranspose -n 5 -max 2000 -m l2_dcm -f $1
 
-./matrixMultiply -a 1d -n 5 -max 2000 -m l1_dcm -f $1
-./matrixMultiply -a 1d -n 5 -max 2000 -m l2_dcm -f $1
+./matrixMultiply -a 1dTranspose -n 5 -max 2200 -m l1_dcm -f $1
+./matrixMultiply -a 1dTranspose -n 5 -max 2200 -m l2_dcm -f $1
 
-./matrixMultiply -a matrixTile -n 5 -max 2000 -m l1_dcm -f $1
-./matrixMultiply -a matrixTile -n 5 -max 2000 -m l2_dcm -f $1
+./matrixMultiply -a 1d -n 5 -max 2200 -m l1_dcm -f $1
+./matrixMultiply -a 1d -n 5 -max 2200 -m l2_dcm -f $1
+
+./matrixMultiply -a matrixTile -n 5 -max 2200 -m l1_dcm -f $1
+./matrixMultiply -a matrixTile -n 5 -max 2200 -m l2_dcm -f $1
+./matrixMultiply -a matrixTile -n 5 -max 2200 -m tot_iis -f $1
